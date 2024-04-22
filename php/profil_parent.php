fe8e5ed868526a795e5897e5d76713e730af319a
nfranni102 <nfranniemmanuel.uta@gmail.com>
2024-04-22 03:29:51 +0000

    Ajout

--- php/profil_parent.php
+++ php/profil_parent.php
@@ -1,9 +1,18 @@
 <?php
+session_start(); // Démarrer la session
+
+// Vérifiez si l'utilisateur est connecté, sinon redirigez vers la page de connexion
+if (!isset($_SESSION['id_parent'])) {
+    header('Location: /html/login_parent.html');
+    exit();
+}
+
 // Connexion à la base de données
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "garderie";
+$id_parent = $_SESSION['id_parent']; // L'ID de l'utilisateur connecté
 
 // Créer la connexion
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -13,22 +22,23 @@ if ($conn->connect_error) {
   die("Échec de la connexion: " . $conn->connect_error);
 }
 
-// Requête SQL pour obtenir les données de la table utilisateur_nounou
-$sql = "SELECT id, ville, nom, prenom, pays, email FROM utilisateur_parend";
-$result = $conn->query($sql);
+// Requête SQL pour obtenir les informations de l'utilisateur connecté
+$sql = "SELECT ville, nom, prenom, pays, email FROM utilisateur_parent WHERE id_parent = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $id_parent);
+$stmt->execute();
+$result = $stmt->get_result();
 
 if ($result->num_rows > 0) {
-  // Afficher les données de chaque ligne
-  while($row = $result->fetch_assoc()) {
-    echo "ID: " . $row["id"] . "<br>";
-    echo "Ville: " . $row["ville"] . "<br>";
-    echo "Nom: " . $row["nom"] . "<br>";
-    echo "Prénom: " . $row["prenom"] . "<br>";
-    echo "Pays: " . $row["pays"] . "<br>";
-    echo "Email: " . $row["email"] . "<br><br>";
-  }
+  // Afficher les informations de l'utilisateur
+  $row = $result->fetch_assoc();
+  echo "Ville: " . $row["ville"] . "<br>";
+  echo "Nom: " . $row["nom"] . "<br>";
+  echo "Prénom: " . $row["prenom"] . "<br>";
+  echo "Pays: " . $row["pays"] . "<br>";
+  echo "Email: " . $row["email"] . "<br>";
 } else {
-  echo "Aucun résultat trouvé.";
+  echo "Aucune information disponible pour l'utilisateur.";
 }
 $conn->close();
 ?>
